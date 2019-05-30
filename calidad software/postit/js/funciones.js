5e42d076d652571cb5b25c398b69a6664241f3fa
andresfiscal <33875317+andresfiscal@users.noreply.github.com>
2019-05-29 23:07:24 -0500

    Add files via upload

--- calidad software/postit/js/funciones.js
+++ calidad software/postit/js/funciones.js
@@ -9,14 +9,23 @@ crearPost.addEventListener('submit', function() {
   if(titulo.length >= 1 && contenido.length >= 1)
   {
     localStorage.setItem(titulo,contenido);
+  }else {
+    if (titulo.length == null ) {
+      alert("ingrese un titulo");
+    }else if (contenido.length == 1) {
+      alert("ingrese un contenido");
+    }
   }
 })
 
+var cambiarColor = document.querySelector("#cambiarColor");
+cambiarColor.addEventListener('', function{
+//  var color = document.querySelector("#crearContenidoNota")
 
+})
 
 var borrarPost = document.querySelector("#removerPost");
 borrarPost.addEventListener('submit', function() {
-
   var titulo = document.querySelector("#deleteTituloNota").value;
   if(titulo.length >= 1)
   {
